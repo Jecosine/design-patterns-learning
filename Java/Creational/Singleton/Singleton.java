48fc5a0fe5e3f62271a5b1b7928bfaa4dfe75441
Jecosine <mj_ol@outlook.com>
2021-01-24 02:40:15 +0800

    UPDATE: Update Singleton implementation

--- Java/Creational/Singleton/Singleton.java
+++ Java/Creational/Singleton/Singleton.java
@@ -1,17 +1,52 @@
 /*
  * @Author: Jecosine
  * @Date: 2021-01-24 02:02:28
- * @LastEditTime: 2021-01-24 02:05:10
+ * @LastEditTime: 2021-01-24 02:39:48
  * @LastEditors: Jecosine
  * @Description: Singleton Implementation
  */
-
-package main;
+import java.io.*;
 
 public class Singleton {
+    public Singleton() {
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("?");
+   }
+}
+
+/**
+ * @description: 
+ * method 1
+ *  lazy initialization [yes]
+ *  thread safe         [yes]
+ */
+class Singleton1 {
+    
+    private static Singleton1 INSTANCE = null;
+
+    private static Singleton1 getInstance() {
+        if (INSTANCE == null)
+            INSTANCE = new Singleton1();
+        return INSTANCE;
+    }
+    
+}
+/**
+ * @description: 
+ * method 2
+ *  lazy initialization [no]
+ *  thread safe         [yes]
+ * 使用了classloader的特性，
+ */
+class Singleton2 {
+    
+    private static final Singleton2 INSTANCE = new Singleton2();
+
+    private static Singleton2 getInstance() {
+        return INSTANCE;
+    }
     
 }
 
-public static void main(String[] args) {
-     
-}
